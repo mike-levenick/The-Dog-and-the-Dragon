b8ba4208971a339ec6272bbaa265f081c0bcb65d
Mike Levenick <michael.levenick@gmail.com>
2024-02-09 22:21:45 -0600

    Improved menu

--- 0-prologue/MainMenu.bash
+++ 0-prologue/MainMenu.bash
@@ -1,8 +1,55 @@
 #!/bin/bash
 
-printmenu() {
+printmainmenu() {
 echo """
-Main Menu. Enter the number for your opion, and press enter.
+                       _____ _                _
+          __          |_   _| |__   ___    __| | ___   __ _
+ \ ______/ v\`_,         | | | '_ \ / _ \  / _\` |/ _ \ / _\` |
+  }        /~~          | | | | | |  __/ | (_| | (_) | (_| | _
+ /_)^ --,r'             |_| |_| |_|\___|  \__,_|\___/ \__, |( )
+|b      |b                                            |___/ |/
+  ___     _____        __      ___  __    _      ___   ___    __
+ ( _ )   /__   \/\  /\/__\    /   \/__\  /_\    / _ \ /___\/\ \ \\
+ / _ \/\   / /\/ /_/ /_\     / /\ / \// //_\\\\  / /_\///  //  \/ /
+| (_>  <  / / / __  //__    / /_// _  \/  _  \/ /_\\\\/ \_// /\  /
+ \___/\/  \/  \/ /_/\__/   /___,'\/ \_/\_/ \_/\____/\___/\_\ \/
+
+
+                                +-----------+
+                                | Main Menu |
+                                +-----------+
+
+Enter the number for your opion, and press enter.
+
+1. New Game
+2. Load Game
+3. Credits
+4. Exit
+
+"""
+}
+
+printretrymenu() {
+echo """
+                       _____ _                _
+          __          |_   _| |__   ___    __| | ___   __ _
+ \ ______/ v\`_,         | | | '_ \ / _ \  / _\` |/ _ \ / _\` |
+  }        /~~          | | | | | |  __/ | (_| | (_) | (_| | _
+ /_)^ --,r'             |_| |_| |_|\___|  \__,_|\___/ \__, |( )
+|b      |b                                            |___/ |/
+  ___     _____        __      ___  __    _      ___   ___    __
+ ( _ )   /__   \/\  /\/__\    /   \/__\  /_\    / _ \ /___\/\ \ \\
+ / _ \/\   / /\/ /_/ /_\     / /\ / \// //_\\\\  / /_\///  //  \/ /
+| (_>  <  / / / __  //__    / /_// _  \/  _  \/ /_\\\\/ \_// /\  /
+ \___/\/  \/  \/ /_/\__/   /___,'\/ \_/\_/ \_/\____/\___/\_\ \/
+
+
+                                +-----------+
+                                | Main Menu |
+                                +-----------+
+
+I'm sorry, that's not a valid option. Please try again.
+Enter the number for your opion, and press enter.
 
 1. New Game
 2. Load Game
@@ -11,16 +58,14 @@ Main Menu. Enter the number for your opion, and press enter.
 """
 }
 
-printmenu
+
+clear
+printmainmenu
 read option
 
 while ! [[ $option =~ ^[1-4]+$ ]]; do
     clear
-    echo "I'm sorry, that's not a valid option. Please try again."
-    printmenu
+    printretrymenu
     read -r option
 done
-
-echo "You entered: $option"
 exit $option
-
