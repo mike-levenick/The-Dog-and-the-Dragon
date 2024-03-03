111c8504a0860ed3ae6034496d85a9529b44669b
Mike Levenick <michael.levenick@gmail.com>
2024-03-03 15:01:19 -0600

    Added 2nd dialogue

--- art/StoryOpenCutscene.sh
+++ art/StoryOpenCutscene.sh
@@ -2,10 +2,9 @@
 
 script_dir="$(dirname "$0")"
 
-
 bash $script_dir/animate.sh $script_dir/dialogue/dialogue1-storyopen.dia 1 print
-sleep 1.5
 bash $script_dir/animate.sh $script_dir/cutscenes/story-open1-enter-barn.anim .05
 sleep 1.5
 bash $script_dir/animate.sh $script_dir/cutscenes/story-open3-exit-barn.anim .05
-sleep 2
+sleep 1.5
+bash $script_dir/animate.sh $script_dir/dialogue/dialogue2-storyopen2.dia 1 print
