d72d4664682f09597df075fe7d9bea019cf25719
Mike Levenick <michael.levenick@gmail.com>
2024-03-13 11:02:13 -0500

    Renamed backup files

---
+++ utils/inventory/UpdateChapter-custom.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# This is a little tool used to update the current chapter.
+# Pass in name as $1 and new chapter as $2.
+
+# Pass in the name as $1, so we can get the correct save file
+name=$1
+
+# Set the new value
+newvalue=$2
+
+# Get the directory of the current script
+script_dir="$(dirname "$0")"
+
+# Get the save file location
+savelocation="$script_dir/../../gamesaves/$name"
+
+# Temp location
+tmplocation="$script_dir/../../gamesaves/.$name-tmp"
+
+# Write new value to temp file
+echo "$newvalue" > "$tmplocation"
+
+# Write inventory data to temp file
+tail -n +2 "$savelocation" >> "$tmplocation"
+
+# Overwrite gamesave with temp file
+mv "$tmplocation" "$savelocation"
+
+echo "⭒Progress saved!⭒"
