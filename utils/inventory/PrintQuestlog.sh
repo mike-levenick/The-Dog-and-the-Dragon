643ab4ec104e7ba3dd3b9d8294960fa5804e3881
Mike Levenick <michael.levenick@gmail.com>
2024-03-14 23:36:07 -0500

    Colors in questlog!

--- utils/inventory/PrintQuestlog.sh
+++ utils/inventory/PrintQuestlog.sh
@@ -23,24 +23,24 @@ echo
 # Get the easy endgame ones out of the way first.
 # Mark - Chapter 4
 if [[ "$chapter" == "4" ]]; then
-    echo "[-] Become a Dragon (Failed)"
+    echo -e "[\e[31m-\e[0m] Become a Dragon (Failed)"
     echo "[ ] Go home"
     exit 0
 fi
 
 # Mark - Chapter 5
 if [[ "$chapter" == "5" ]]; then
-    echo "[-] Become a Dragon (Failed)"
-    echo "[X] Go home"
+    echo -e "[\e[31m-\e[0m] Become a Dragon (Failed)"
+    echo -e "[\e[32mX\e[0m] Go home"
     echo "[ ] Save the farmer's son!"
     exit 0
 fi
 
 # MARK - Chapter 6
 if [[ "$chapter" == "6" ]]; then
-    echo "[-] Become a Dragon (Failed)"
-    echo "[X] Go home"
-    echo "[X] Save the farmer's son!"
+    echo -e "[\e[31m-\e[0m] Become a Dragon (Failed)"
+    echo -e "[\e[32mX\e[0m] Go home"
+    echo -e "[\e[32mX\e[0m] Save the farmer's son!"
     echo "[ ] Rest by the fireplace"
     exit 0
 fi
@@ -57,19 +57,19 @@ if [[ "$chapter" == "1" || "$chapter" == "1a" || "$chapter" == "1b" || "$chapter
         echo "      [ ] Harvest seeds"
         echo "      [ ] Roll in seeds"
     elif [[ "$chapter" == "1b" ]]; then
-        echo "      [X] Plant seeds"
+        echo -e "      [\e[32mX\e[0m] Plant seeds"
         echo "      [ ] Water seeds"
         echo "      [ ] Harvest seeds"
         echo "      [ ] Roll in seeds"
     elif [[ "$chapter" == "1c" ]]; then
-        echo "      [X] Plant seeds"
-        echo "      [X] Water seeds"
+        echo -e "      [\e[32mX\e[0m] Plant seeds"
+        echo -e "      [\e[32mX\e[0m] Water seeds"
         echo "      [ ] Harvest seeds"
         echo "      [ ] Roll in seeds"
     elif [[ "$chapter" == "1d" ]]; then
-        echo "      [X] Plant seeds"
-        echo "      [X] Water seeds"
-        echo "      [X] Harvest seeds"
+        echo -e "      [\e[32mX\e[0m] Plant seeds"
+        echo -e "      [\e[32mX\e[0m] Water seeds"
+        echo -e "      [\e[32mX\e[0m] Harvest seeds"
         echo "      [ ] Roll in seeds"
     fi
     echo "   [ ] Learn to talk"
@@ -78,19 +78,19 @@ fi
 
 # Mark - Chapter 2
 if [[ "$chapter" == "2" || "$chapter" == "2a" || "$chapter" == "2b" || "$chapter" == "2c" ]]; then
-    echo "   [X] Get scales"
+    echo -e "   [\e[32mX\e[0m] Get scales"
     echo "   [ ] Learn to talk"
     if [[ "$chapter" == "2a" ]]; then
         echo "      [ ] Find a language book"
         echo "      [ ] Read the book with the farmer's son"
         echo "      [ ] Learn to say \"hello world\""
     elif [[ "$chapter" == "2b" ]]; then
-        echo "      [X] Find a language book"
+        echo -e "      [\e[32mX\e[0m] Find a language book"
         echo "      [ ] Read the book with the farmer's son"
         echo "      [ ] Learn to say \"hello world\""
     elif [[ "$chapter" == "2c" ]]; then
-        echo "      [X] Find a language book"
-        echo "      [X] Read the book with the farmer's son"
+        echo -e "      [\e[32mX\e[0m] Find a language book"
+        echo -e "      [\e[32mX\e[0m] Read the book with the farmer's son"
         echo "      [ ] Learn to say \"hello world\""
     fi
     echo "   [ ] Learn to fly"
@@ -98,20 +98,20 @@ fi
 
 # Mark - Chapter 3
 if [[ "$chapter" == "3" || "$chapter" == "3a" || "$chapter" == "3b" || "$chapter" == "3c" ]]; then
-    echo "   [X] Get scales"
-    echo "   [X] Learn to talk"
+    echo -e "   [\e[32mX\e[0m] Get scales"
+    echo -e "   [\e[32mX\e[0m] Learn to talk"
     echo "   [ ] Learn to fly"
     if [[ "$chapter" == "3a" ]]; then
         echo "      [ ] Study pulley system"
         echo "      [ ] Adjust counterweight"
         echo "      [ ] Fly!"
     elif [[ "$chapter" == "3b" ]]; then
-        echo "      [X] Study pulley system"
+        echo -e "      [\e[32mX\e[0m] Study pulley system"
         echo "      [ ] Adjust counterweight"
         echo "      [ ] Fly!"
     elif [[ "$chapter" == "3c" ]]; then
-        echo "      [X] Study pulley system"
-        echo "      [X] Adjust counterweight"
+        echo -e "      [\e[32mX\e[0m] Study pulley system"
+        echo -e "      [\e[32mX\e[0m] Adjust counterweight"
         echo "      [ ] Fly!"
     fi
 fi
